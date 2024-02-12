abda4441296257d8784706234f75cc02bc0c84ef
Jeremy Badger <jeremebadger@gmail.com>
2024-02-12 14:54:16 -0800

    Removed new page and random button assigning random values to saturation and lightness, now fixed at 50%

--- src/components/ColorSlider.tsx
+++ src/components/ColorSlider.tsx
@@ -5,12 +5,9 @@ export function ColorSlider() {
   const [hueValue, setHue] = useState<string | number>(
     Math.floor(Math.random() * 361)
   )
-  const [saturationValue, setSaturation] = useState<string | number>(
-    Math.floor(Math.random() * 101)
-  )
-  const [lightnessValue, setLightness] = useState<string | number>(
-    Math.floor(Math.random() * 101)
-  )
+  const [saturationValue, setSaturation] = useState<string | number>(50)
+
+  const [lightnessValue, setLightness] = useState<string | number>(50)
 
   const newBackgroundColor = `hsl(${hueValue},${saturationValue}%,${lightnessValue}%)`
   const newStyle = {
@@ -19,8 +16,8 @@ export function ColorSlider() {
 
   function handleButton() {
     setHue(Math.floor(Math.random() * 361))
-    setSaturation(Math.floor(Math.random() * 101))
-    setLightness(Math.floor(Math.random() * 101))
+    setSaturation(50)
+    setLightness(50)
   }
 
   return (
@@ -29,72 +26,74 @@ export function ColorSlider() {
         <h1>HSL Color Picker</h1>
       </header>
       <main>
-        <aside id="colorBox" style={newStyle}></aside>
-        <section id="sliders" className="sliderStyle">
-          <article id="hueSlider">
-            <label htmlFor="hue" className="label">
-              H
-            </label>
-            <input
-              className="hue"
-              type="range"
-              id="hue"
-              name="hue"
-              min="0"
-              max="360"
-              value={hueValue}
-              onChange={(event) => setHue(event.target.value)}
-            />
-            <input
-              className="textBox"
-              type="text"
-              value={hueValue}
-              maxLength={3}
-            />
-          </article>
-          <article id="saturationSlider">
-            <label htmlFor="saturation" className="label">
-              S
-            </label>
-            <input
-              className="saturation"
-              type="range"
-              id="saturation"
-              name="saturation"
-              min="0"
-              max="100"
-              value={saturationValue}
-              onChange={(event) => setSaturation(event.target.value)}
-            />
-            <input
-              className="textBox"
-              type="text"
-              value={saturationValue}
-              maxLength={3}
-            />
-          </article>
-          <article id="lightnessSlider">
-            <label htmlFor="lightness" className="label">
-              L
-            </label>
-            <input
-              className="lightness"
-              type="range"
-              id="lightness"
-              name="lightness"
-              min="0"
-              max="100"
-              value={lightnessValue}
-              onChange={(event) => setLightness(event.target.value)}
-            />
-            <input
-              className="textBox"
-              type="text"
-              value={lightnessValue}
-              maxLength={3}
-            />
-          </article>
-        </section>
+        <div className="grouping">
+          <aside id="colorBox" style={newStyle}></aside>
+          <section id="sliderContainer">
+            <article className="slider">
+              <label htmlFor="hue" className="label">
+                H
+              </label>
+              <input
+                className="hue"
+                type="range"
+                id="hue"
+                name="hue"
+                min="0"
+                max="360"
+                value={hueValue}
+                onChange={(event) => setHue(event.target.value)}
+              />
+              <input
+                className="textBox"
+                type="text"
+                value={hueValue}
+                maxLength={3}
+              />
+            </article>
+            <article className="slider">
+              <label htmlFor="saturation" className="label">
+                S
+              </label>
+              <input
+                className="saturation"
+                type="range"
+                id="saturation"
+                name="saturation"
+                min="0"
+                max="100"
+                value={saturationValue}
+                onChange={(event) => setSaturation(event.target.value)}
+              />
+              <input
+                className="textBox"
+                type="text"
+                value={saturationValue}
+                maxLength={3}
+              />
+            </article>
+            <article className="slider">
+              <label htmlFor="lightness" className="label">
+                L
+              </label>
+              <input
+                className="lightness"
+                type="range"
+                id="lightness"
+                name="lightness"
+                min="0"
+                max="100"
+                value={lightnessValue}
+                onChange={(event) => setLightness(event.target.value)}
+              />
+              <input
+                className="textBox"
+                type="text"
+                value={lightnessValue}
+                maxLength={3}
+              />
+            </article>
+          </section>
+        </div>
       </main>
       <footer>
         <button onClick={handleButton}>New Random Color</button>
